bacf1fb878c0ebebaa7292d32e9e18d4838a054a
MohammedAlMaqbli <55395499+MohammedAlMaqbli@users.noreply.github.com>
2019-11-24 10:52:46 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,42 +1,54 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-const breedUrl = 'https://dog.ceo/api/breeds/list/all';
-function fetchImages() {
-    fetch(imgUrl).then(function (response) {
-        return response.json();
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+const getImages = async () => {
+  const imgDiv = document.querySelector("#dog-image-container");
+  const response = await fetch(imgUrl);
+  const data = await response.json();
+  data.message.map(img => {
+    const newImgElement = document.createElement("img");
+    newImgElement.setAttribute("src", img);
+    newImgElement.style.width = "200px";
+    newImgElement.style.margin = "10px";
+    imgDiv.appendChild(newImgElement);
+  });
+};
 
-    }).then(function (json) {
-        const imgDiv = document.getElementById('dog-image-container');
-        for (const key of json) {
-            const newImgElement = document.createElement("img");
-            newImgElement.setAttribute("src", key);
-            document.body.appendChild(newImgElement);
-            imgDiv.appendChild(newImgElement);
-        }
-    })
-}
-function fetchDogs {
-    fetch(breedUrl).then(function (response) {
-        return response.json();
-    }).then(function (json) {
-        const dogsDiv = document.getElementById('dog-breeds');
-        let counter = 0 ; 
-        for (const element of json) {
-            const newLiElement = document.createElement('li');
-            newLiElement.innerHTML= element;
-            newLiElement.setAttribute("id" ,"dog-" + counter );
-            dogsDiv.appendChild(newLiElement);
-        }
-    })
-}
-function changeTextOnClick() {
-    const liElements = document.querySelectorAll("#dog-breeds li");
-    liElements.addEventListener('click', function () {
-        let liElementId = liElements.id 
-    })
-}
-document.addEventListener("DOMContentLoaded", function () {
-    fetchImages();
-    fetchDogs();
-    
+const getBreeds = async () => {
+  const response = await fetch(breedUrl);
+  const data = await response.json();
+  const dogsDiv = document.getElementById("dog-breeds");
+  let counter = 0;
+  for (const key in data.message) {
+    const newLiElement = document.createElement("li");
+    newLiElement.innerHTML = key;
+    newLiElement.setAttribute("id", "dog-" + counter);
+    dogsDiv.appendChild(newLiElement);
+    counter++;
+  }
+};
+const changeColor = () => {
+  document.getElementById("dog-breeds").addEventListener("click", e => {
+    e.target.tagName === "LI" ? (e.target.style.color = "firebrick") : null;
+  });
+};
+const filterList = char => {
+  const dogsDiv = document.getElementById("dog-breeds");
+  Array.from(dogsDiv.children)
+    .filter(dog => dog.textContent.toLocaleLowerCase().charAt(0) !== char)
+    .forEach(dog => dog.classList.add("hide"));
+  Array.from(dogsDiv.children)
+    .filter(dog => dog.textContent.toLocaleLowerCase().charAt(0) === char)
+    .forEach(dog => dog.classList.remove("hide"));
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  getImages();
+  getBreeds();
+  changeColor();
+  document.getElementById("breed-dropdown").addEventListener("click", e => {
+    filterList(e.target.value);
+  });
 });
+
+"moh".charAt(0);
